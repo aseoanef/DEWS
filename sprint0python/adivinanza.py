34ae8201e3a946f81d1561d9346de9c48faeb678
aseoanef23@fpcoruna.afundacion.org <aseoanef23@fpcoruna.afundacion.org>
2023-09-27 10:50:50 +0200

    ejercicio 1,2,3 hechos

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,20 @@
+respuesta = input("adivinanza \n a. \n b. \n c. \n")
+score = 0
+if respuesta == "a":
+    print("Congrats")
+    score= score+1
+else:
+    print("Dumbass")
+respuesta = input("adivinanza \n a. \n b. \n c. \n")
+if respuesta == "b":
+    print("Congrats")
+    score= score+1
+else:
+    print("Dumbass") 
+respuesta = input("adivinanza \n a. \n b. \n c. \n")
+if respuesta == "c":
+    print("Congrats")
+    score= score+1
+else:
+    print("Dumbass")
+print("your score is :",score)       
---
+++ sprint0python/operaciones.py
@@ -0,0 +1,17 @@
+select = input("1-suma\n2-resta\n3-multiplicacion\n4-division\n")
+operator1 = input("operator 1: ")
+operator2 = input("operator 2: ")
+result = 0
+if operator2=="0":
+        print("operator 2 cant be 0")
+elif select=="1":
+    result=int(operator1) + int(operator2)
+elif select=="2":
+    result=int(operator1) + int(operator2)
+elif select=="3":
+    result=int(operator1) + int(operator2)
+elif select=="4":
+    result=int(operator1) + int(operator2)
+print(result)
+    
+
