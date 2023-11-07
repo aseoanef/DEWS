4b5e9fc06be07c3dfec9ddbbac608b34078539ae
aseoanef23@fpcoruna.afundacion.org <aseoanef23@fpcoruna.afundacion.org>
2023-11-07 13:12:42 +0100

    Añadido boton y detail activity tarea 3.1.2

---
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -0,0 +1,26 @@
+package com.example.mycatalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CatalogActivity extends AppCompatActivity {
+    private Context context=this;
+    private Button detailButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.catalog_activity);
+        detailButton = findViewById(R.id.detailButton);
+        detailButton.setOnClickListener(view -> {
+            Intent intent = new Intent(context, DetailActivity.class);
+            context.startActivity(intent);
+        });
+
+
+    }
+}
