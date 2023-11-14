e35b202ebde8d394347f31517a4ae73e5f2761c8
aseoanef23@fpcoruna.afundacion.org <aseoanef23@fpcoruna.afundacion.org>
2023-11-14 13:26:54 +0100

    Añadido layout landscape Tarea 3.1.4

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -2,6 +2,7 @@ package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
@@ -9,6 +10,13 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.detail_activity);
+        switch (getResources().getConfiguration().orientation) {
+            case Configuration.ORIENTATION_PORTRAIT:
+                setContentView(R.layout.detail_activity);
+                break;
+            case Configuration.ORIENTATION_LANDSCAPE:
+                setContentView(R.layout.detail_land);
+                break;
+        }
     }
 }
